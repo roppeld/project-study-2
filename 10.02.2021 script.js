51259462398d5e67030d39c88828fed76cddcd11
Margarita <roppeldmargo@gmail.com>
2021-02-11 21:25:32 +0200

    11.02.2021 episode I.III

--- 10.02.2021 script.js
+++ 10.02.2021 script.js
@@ -2,4 +2,17 @@
 
 const btns = document.querySelectorAll('button');
 
-console.log(btns[0].classList.length);
+// console.log(btns[0].classList.length);
+console.log(btns[0].classList.item(0));//получаем класс
+console.log(btns[1].classList.add('red'));
+//если добавлен класс, который в css имеет свой код, то это создание
+// интерактивных вещей и возможность динамически менять классы на странице
+
+// console.log(btns[0].classList.remove('blue'));
+console.log(btns[0].classList.toggle('blue')); //тоглить классы - значит
+//переключать их, если нет такого класса, то он добавиться, если есть
+// - уберется
+
+if (btns[1].classList.contains('red')) {
+    console.log('red');
+}
