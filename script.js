35551b943bbb7a897612a3b7a2a5543cc1234149
Margarita Roppeld <roppeldmargo@gmail.com>
2021-02-16 19:59:39 +0200

    039 lesson

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 "use script";
 
+// const timerId = setTimeout(function() {
+//         console.log("Hello");
+// }, 2000);
+
+
+const btn = document.querySelector('.btn');
+let timerId;
+
+btn.addEventListener('click', () => {
+    // const timerId = setTimeout(logger, 2000);
+    const timerId = setInterval(logger, 2000);
+});
+
+clearInterval(timerId);//сбрасывает счётчик
+
+function logger () {
+    console.log('text');
+}
