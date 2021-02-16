4160597f22b035afbfc7d3402ff8943fd5a20228
Margarita Roppeld <roppeldmargo@gmail.com>
2021-02-16 20:24:22 +0200

    recursion setTimeout

--- script.js
+++ script.js
@@ -6,15 +6,19 @@
 
 
 const btn = document.querySelector('.btn');
-let timerId;
+let timerId,
+    i = 0;
 
 btn.addEventListener('click', () => {
     // const timerId = setTimeout(logger, 2000);
-    const timerId = setInterval(logger, 2000);
+    timerId = setInterval(logger, 500);
 });
 
-clearInterval(timerId);//сбрасывает счётчик
-
 function logger () {
+    if (i === 3) {
+       clearInterval(timerId);//сбрасывает счётчик
+    }
     console.log('text');
+    i++;
 }
+
