a4d96a623e107f7d1befbe3766d7009fee2fdab9
Margarita Roppeld <roppeldmargo@gmail.com>
2021-02-17 18:43:45 +0200

    Date lesson 40

---
+++ 040 script.js
@@ -0,0 +1,32 @@
+"use script";
+
+const now = new Date();
+
+console.log(now);
+
+const now2 = new Date();
+
+console.log(now2.getFullYear());
+console.log(now2.getMonth());
+console.log(now2.getDate());
+console.log(now2.getHours());
+console.log(now2.getMinutes());
+console.log(now2.getSeconds());
+
+
+
+// const date = new Date('2021-02-17');
+
+// console.log(date);
+
+// const date2 = new Date(2021, 2, 17, 17, 56);
+
+// console.log(date2);
+
+// const date3 = new Date(0);
+
+// console.log(date3);
+
+// const date4 = new Date(-85677098965);
+
+// console.log(date4);
