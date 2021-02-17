9f12cc0e5484ce9533b92854e43220e5eba2b846
Margarita Roppeld <roppeldmargo@gmail.com>
2021-02-17 17:28:12 +0200

    animation

--- script.js
+++ script.js
@@ -9,16 +9,30 @@ const btn = document.querySelector('.btn');
 let timerId,
     i = 0;
 
-btn.addEventListener('click', () => {
-    // const timerId = setTimeout(logger, 2000);
-    timerId = setInterval(logger, 500);
-});
+    function myAnimation() {
+        const element = document.querySelector('.box');
+        let position = 0;
 
-function logger () {
-    if (i === 3) {
-       clearInterval(timerId);//сбрасывает счётчик
+       const id = setInterval(frame, 5);
+
+        function frame() {
+            if (position == 300) {
+                clearInterval(id);
+            } else {
+                position++;
+                element.style.top = position + 'px';
+                element.style.left = position + 'px';
+            } 
+        }
     }
-    console.log('text');
-    i++;
-}
+
+btn.addEventListener('click', myAnimation);
+
+// function logger () {
+//     if (i === 3) {
+//        clearInterval(timerId);//сбрасывает счётчик
+//     }
+//     console.log('text');
+//     i++;
+// }
 
