09820ad85b58c864bc24b5a61492247a4d7b86f0
Margarita <roppeldmargo@gmail.com>
2021-02-24 19:12:30 +0200

    extends part

--- 047 lesson Class script.js
+++ 047 lesson Class script.js
@@ -11,6 +11,23 @@ class Rectangle {
     }
 }
 
+class ColoredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст: "${this.text}", цвет: ${this.bgColor}`);
+    }
+}
+
+const div = new ColoredRectangleWithText(30, 50, "I'am Rectangle!", 'green');
+
+div.showMyProps();
+console.log(div.calcArea());
+
 const long = new Rectangle(20, 100);
 const square = new Rectangle(10, 10);
 
