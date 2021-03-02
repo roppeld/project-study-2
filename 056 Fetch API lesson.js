5b9d465fa369e5e59528ebfaebb9f5c085fa3be0
Margarita <roppeldmargo@gmail.com>
2021-03-02 16:22:06 +0200

    Fetch study continue

--- 056 Fetch API lesson.js
+++ 056 Fetch API lesson.js
@@ -4,4 +4,8 @@
     //которые можно использовать. DOM API, то, с чем уже встречалась - встроенные методы в браузер для работы с 
     //DOM-структурой на странице; Fetch тоже встроен в браузер
 
-    
+    //JSONPlaceholder -небольшая база даны в формате JSONж фейковый REST API. 
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())//json() - метод уже переформатирует всё в js; также вс это возвращает promise
+    .then(json => console.log(json));
