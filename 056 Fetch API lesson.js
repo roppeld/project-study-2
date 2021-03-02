439eddc60cd592240c8f87b3b39d941633f8d965
Margarita <roppeldmargo@gmail.com>
2021-03-02 16:44:20 +0200

    056 lesson Fetch  done

--- 056 Fetch API lesson.js
+++ 056 Fetch API lesson.js
@@ -4,8 +4,14 @@
     //которые можно использовать. DOM API, то, с чем уже встречалась - встроенные методы в браузер для работы с 
     //DOM-структурой на странице; Fetch тоже встроен в браузер
 
-    //JSONPlaceholder -небольшая база даны в формате JSONж фейковый REST API. 
+    //JSONPlaceholder -небольшая база даны в формате JSON; фейковый REST API. 
 
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())//json() - метод уже переформатирует всё в js; также вс это возвращает promise
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: "POST",//в объекте задаются настройки запросу 
+        body: JSON.stringify({name: 'Alex'}),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+    .then(response => response.json())
     .then(json => console.log(json));
